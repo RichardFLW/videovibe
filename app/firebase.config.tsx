33e512252709cf12f2ed06a5d4a967675e12bde8
RichardFLW <richard.windels@icloud.com>
2024-05-24 15:32:33 +0200

    version 4

--- app/firebase.config.tsx
+++ app/firebase.config.tsx
@@ -5,13 +5,7 @@ import { getStorage } from "firebase/storage";
  
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBp_y6wYbdcxpg2tUhud1SAA-LHbUuZUDI",
-  authDomain: "videovibe-78ddb.firebaseapp.com",
-  databaseURL:"https://videovibe-78ddb-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "videovibe-78ddb",
-  storageBucket: "videovibe-78ddb.appspot.com",
-  messagingSenderId: "279583705221",
-  appId: "1:279583705221:web:86f901f995c1d04e6741ec",
+//info de config
 };
 
 
