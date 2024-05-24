f92efb67fe565e1c4f5b5cbe5a533af8a041dcee
RichardFLW <richard.windels@icloud.com>
2024-05-24 13:41:09 +0200

    version 2

--- app/firebase.config.tsx
+++ app/firebase.config.tsx
@@ -3,7 +3,17 @@ import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage"; 
  
-import firebaseConfig from "./firebaseSecrets";
+const firebaseConfig = {
+  apiKey: "VOTRE_API_KEY",
+  authDomain: "VOTRE_AUTH_DOMAIN",
+  databaseURL: "https://VOTRE_DB_URL",
+  projectId: "VOTRE_PROJECT_ID",
+  storageBucket: "VOTRE_STORAGE_BUCKET",
+  messagingSenderId: "VOTRE_MESSAGING_SENDER_ID",
+  appId: "VOTRE_APP_ID",
+};
+
+export default firebaseConfig;
 
 
 const app = initializeApp(firebaseConfig);
